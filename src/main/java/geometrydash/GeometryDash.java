d0ed6d96f18b98c5094812534482721081b7a450
clayton-chung <claytonjames69420@gmail.com>
2023-10-04 21:50:27 -0700

    completed exercise

--- src/main/java/geometrydash/GeometryDash.java
+++ src/main/java/geometrydash/GeometryDash.java
@@ -1,5 +1,6 @@
 package geometrydash;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class GeometryDash {
@@ -10,8 +11,30 @@ public class GeometryDash {
      * @return true if the play completes the level and false otherwise
      */
     public static boolean isSuccessfulPlay(String level, String play) {
-        // TODO: Implement this method
-        return false;
+        char[] levelArray = level.toCharArray();
+        char[] playArray = play.toCharArray();
+
+        if (levelArray[0] == '^' || levelArray[levelArray.length-1] == '^'){
+            return false;
+        }
+
+        int i = 0;
+        for (char move : playArray) {
+            if (move == '1' && i < levelArray.length - 1) {
+                i += 1;
+            } else if (move == '2' && i < levelArray.length - 2) {
+                i += 2;
+            } else if (move == '3' && i < levelArray.length - 3) {
+                i += 3;
+            } else if (move == '0') {
+            } else {
+                return false;
+            }
+            if (levelArray[i] == '^') {
+                return false;
+            }
+        }
+        return true;
     }
 
     /**
@@ -26,8 +49,53 @@ public class GeometryDash {
      */
     public static Set<String> successfulPlays(String level, Set<String> possiblePlays,
                                               int startingEnergy, int targetRestingEnergy) {
-        // TODO: Implement this method
-        return null;
+        Set<String> success = new HashSet<>();
+
+        for (String play : possiblePlays) {
+            if (valid(level, play, startingEnergy, targetRestingEnergy)) {
+                success.add(play);
+            }
+        }
+
+        return success;
+    }
+
+    private static boolean valid (String level, String possiblePlay,
+                                 int energy, int targetRestingEnergy){
+
+        char[] levelArray = level.toCharArray();
+        char[] playArray = possiblePlay.toCharArray();
+
+        if (levelArray[0] == '^' || levelArray[levelArray.length-1] == '^'){
+            return false;
+        }
+
+        int i = 0;
+        for (char move : playArray) {
+            if (move == '1' && i < levelArray.length - 1) {
+                i += 1;
+                energy-=1;
+            } else if (move == '2' && i < levelArray.length - 2) {
+                i += 2;
+                energy-=2;
+            } else if (move == '3' && i < levelArray.length - 3) {
+                i += 3;
+                energy-=3;
+            } else if (move == '0' && energy < 3) {
+                energy += 1;
+            } else if (move == '0'){
+                // do nothing
+            } else {
+                return false;
+            }
+
+            if (levelArray[i] == '*') {
+                i+=4;
+            } else if (levelArray[i] == '^' || energy < 0){
+                return false;
+            }
+        }
+        return energy>=targetRestingEnergy && i == levelArray.length-1;
     }
 
     /**
@@ -40,8 +108,48 @@ public class GeometryDash {
      */
     public static String shortestPlay(String level, int startingEnergy, int targetRestingEnergy)
             throws UnplayableLevelException {
-        // TODO: Implement this method
-        return null;
+        String shortest = generatePlay(level, startingEnergy, targetRestingEnergy, "",0);
+        if (shortest.equals("N/A")){
+            throw new UnplayableLevelException();
+        }
+        return shortest;
+    }
+
+    private static String generatePlay(String level, int energy, int targetEnergy, String play, int levelIndex){
+        if (levelIndex >= level.length()) {
+            return "N/A";
+        }
+
+        if (levelIndex == level.length()-1){
+            if (energy>=targetEnergy){
+                return play;
+            } else {
+                return "N/A";
+            }
+        }
+
+        if (energy < 0){
+            return "N/A";
+        }
+
+        if (level.charAt(levelIndex) == '*') {
+            return generatePlay(level, energy, targetEnergy, play, levelIndex + 4);
+        } else if (level.charAt(levelIndex) == '^'){
+            return "N/A";
+        }
+
+        String play0 = energy < 3? generatePlay(level, energy+1, targetEnergy, play+"0", levelIndex):"N/A";
+        String play1 = generatePlay(level, energy-1, targetEnergy, play+"1", levelIndex+1);
+        String play2 = generatePlay(level, energy-2, targetEnergy, play+"2", levelIndex+2);
+        String play3 = generatePlay(level, energy-3, targetEnergy, play+"3", levelIndex+3);
+
+        String shortest = "N/A";
+        for (String plays : new String[]{play0,play1,play2,play3}){
+            if(!plays.equals("N/A") && (shortest.equals("N/A") || plays.length() < shortest.length())){
+                shortest = plays;
+            }
+        }
+        return shortest;
     }
 
     /**
@@ -53,7 +161,33 @@ public class GeometryDash {
      * with target resting energy {@code targetRestingEnergy}
      */
     public static int numberOfPlays(String level, int startingEnergy, int targetRestingEnergy) {
-        // TODO: Implement this method
-        return -1;
+        return countPlays(level, startingEnergy, targetRestingEnergy, 0);
+    }
+
+    private static int countPlays(String level, int energy, int targetEnergy, int levelIndex) {
+        if (levelIndex >= level.length()) {
+            return 0;
+        }
+
+        if (levelIndex == level.length() - 1) {
+            return (energy >= targetEnergy) ? 1 : 0;
+        }
+
+        if (energy < 0) {
+            return 0;
+        }
+
+        if (level.charAt(levelIndex) == '*') {
+            return countPlays(level, energy, targetEnergy, levelIndex + 4);
+        } else if (level.charAt(levelIndex) == '^') {
+            return 0;
+        }
+
+        int count0 = energy < 3 ? countPlays(level, energy + 1, targetEnergy, levelIndex) : 0;
+        int count1 = countPlays(level, energy - 1, targetEnergy, levelIndex + 1);
+        int count2 = countPlays(level, energy - 2, targetEnergy, levelIndex + 2);
+        int count3 = countPlays(level, energy - 3, targetEnergy, levelIndex + 3);
+
+        return count0 + count1 + count2 + count3;
     }
 }
